f07ea8de45eae57e5f5000553a1f9f90dfaffc4d
krzysio <krzysio@brzoskwinka.home>
2018-09-27 23:55:51 +0200

    zmodyfikowano przypadek testowy  Committer: krzysio <krzysio@brzoskwinka.home>
     On branch efekty_graficzne
     Changes to be committed:
    	modified:   zaznaczenie_test.py

--- zaznaczenie_test.py
+++ zaznaczenie_test.py
@@ -1,28 +1,28 @@
 import pygame
 import zaznaczenie
 
-def funkcja_zwrotna(drugi_przycisk):
+def funkcja_zwrotna(drugi_przycisk, trzeci_przycisk):
     print("Zaznaczono")
     print(drugi_przycisk)
-    if drugi_przycisk.reprezentacja:
+    if drugi_przycisk.reprezentacja or trzeci_przycisk.reprezentacja:
         print("czyszczę drugi")
         drugi_przycisk.wyczysc()
-
-def funkcja_zwrotna2(drugi_przycisk):
-    print("Zaznaczono 2")
+        trzeci_przycisk.wyczysc()
 
 if __name__ == "__main__":
     SURFACE = pygame.display.set_mode((800, 600))
-    przycisk = zaznaczenie.PrzyciskGraf()
+    przycisk1 = zaznaczenie.PrzyciskGraf()
     przycisk2 = zaznaczenie.PrzyciskGraf()
-    przycisk.dodaj_obsluge("przycisk1", funkcja_zwrotna, przycisk2)
-    przycisk2.dodaj_obsluge("przycisk2", funkcja_zwrotna, przycisk)
-    przycisk.umiesc_na_pozycji(50, 50, SURFACE)
+    przycisk3 = zaznaczenie.PrzyciskGraf()
+    przycisk1.dodaj_obsluge("przycisk1", funkcja_zwrotna, przycisk2, przycisk3)
+    przycisk2.dodaj_obsluge("przycisk2", funkcja_zwrotna, przycisk1, przycisk3)
+    przycisk3.dodaj_obsluge("przycisk2", funkcja_zwrotna, przycisk1, przycisk2)
+    przycisk1.umiesc_na_pozycji(50, 50, SURFACE)
     przycisk2.umiesc_na_pozycji(50, 150, SURFACE)
+    przycisk3.umiesc_na_pozycji(50, 200, SURFACE)
     zegar = pygame.time.Clock()
     while True:
         events = pygame.event.get()
-        przycisk.wykryj_klikniecie(events)
-        #zegar.tick(15)
+        przycisk1.wykryj_klikniecie(events)
         przycisk2.wykryj_klikniecie(events)
-        #zegar.tick(15)
+        przycisk3.wykryj_klikniecie(events)
