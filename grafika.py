ac899afc6978966c1074e31e3c1b2557edd615fa
krzysio <krzysio@brzoskwinka.home>
2018-10-13 01:28:51 +0200

    poprawiono wychodzenie symboli poza linie  Committer: krzysio <krzysio@brzoskwinka.home>
     On branch poprawka_eg
     Changes to be committed:
    	modified:   grafika.py

--- grafika.py
+++ grafika.py
@@ -6,6 +6,7 @@ import sys
 
 # stałe
 WIELKOSC = 20  # liczba pixeli przypadających na komórkę
+odleglosc = WIELKOSC + 1
 POCZATEK = (50, 50)
 WIERSZ = 0
 KOLUMNA = 1
@@ -18,10 +19,8 @@ class Symbol_graf:
     """graficzna reprezentacja x lub o"""
 
     def rysuj_na_pozycji(self, pozycja, surface):
-        poz_x = POCZATEK[KOLUMNA] + pozycja[KOLUMNA] * WIELKOSC
-        + pozycja[KOLUMNA] + 1
-        poz_y = POCZATEK[WIERSZ] + pozycja[WIERSZ] * WIELKOSC
-        + pozycja[WIERSZ] + 1
+        poz_x = POCZATEK[KOLUMNA] + pozycja[KOLUMNA] * dlugosc
+        poz_y = POCZATEK[WIERSZ] + pozycja[WIERSZ] * dlugosc
         surface.blit(self.sym, (poz_x, poz_y))
         pygame.display.flip()
 
@@ -34,12 +33,14 @@ class Kolko_graf(Symbol_graf):
     def __init__(self):
         super().__init__()
         self.sym = self.font.render('o', False, (0, 255, 0))
+        self.sym = pygame.transform.scale(self.sym, (WIELKOSC, WIELKOSC))
 
 
 class Krzyzyk_graf(Symbol_graf):
     def __init__(self):
         super().__init__()
         self.sym = self.font.render('x', False, (0, 255, 0))
+        self.sym = pygame.transform.scale(self.sym, (WIELKOSC, WIELKOSC))
 
 
 class Puste_graf(Symbol_graf):
@@ -51,10 +52,8 @@ reprezentacja = {symbol.Puste: Puste_graf(), symbol.Kolko: Kolko_graf(),
 
 
 def rysuj_na_pozycji(symbol, pozycja, surface):
-        poz_x = POCZATEK[KOLUMNA] + pozycja[KOLUMNA] * WIELKOSC
-        + pozycja[KOLUMNA] + 1
-        poz_y = POCZATEK[WIERSZ] + pozycja[WIERSZ] * WIELKOSC
-        + pozycja[WIERSZ] + 1
+        poz_x = POCZATEK[KOLUMNA] + pozycja[KOLUMNA] * odleglosc
+        poz_y = POCZATEK[WIERSZ] + pozycja[WIERSZ] * odleglosc
         surface.blit(reprezentacja[symbol].sym, (poz_x, poz_y))
         pygame.display.flip()
 
@@ -70,32 +69,33 @@ def rysuj_obwodke(wielkosc_obszaru, surface):
     pygame.draw.rect(surface, CZERWONY, obwodka, 1)
 
 
+def _linia(poczatek, koniec, surface):
+    pygame.draw.line(surface, CZERWONY, poczatek, koniec)
+
 def rysuj_linie_poziome(wiersze, surface):
-    pozx_pocz = POCZATEK[0]
-    pozx_koniec = pozx_pocz + wiersze * WIELKOSC + wiersze
-    y = POCZATEK[1] + WIELKOSC + 1
+    pozx_pocz = POCZATEK[WIERSZ]
+    pozx_koniec = pozx_pocz + wiersze * odleglosc
+    y = POCZATEK[KOLUMNA]
     for p in range(1, wiersze):
-        pygame.draw.line(surface, CZERWONY,
-                         (pozx_pocz, y), (pozx_koniec, y))
-        y += WIELKOSC + 1
+        yn = y + p * odleglosc
+        _linia((pozx_pocz, yn), (pozx_koniec, yn), surface)
 
 
 def rysuj_linie_pionowe(kolumny, surface):
-    pozy_pocz = POCZATEK[1]
-    pozy_koniec = pozy_pocz + kolumny * WIELKOSC + kolumny
-    x = POCZATEK[0] + WIELKOSC + 1
+    pozy_pocz = POCZATEK[KOLUMNA]
+    pozy_koniec = pozy_pocz + kolumny * odleglosc
+    x = POCZATEK[WIERSZ]
     for p in range(1, kolumny):
-        pygame.draw.line(surface, CZERWONY,
-                         (x, pozy_pocz), (x, pozy_koniec))
-        x += WIELKOSC + 1
+        xn = x + p * odleglosc
+        _linia((xn, pozy_pocz), (xn, pozy_koniec), surface)
 
 
 def rysuj_siatke(plansza_rozmiar, surface):
     """ Rysuje plansze
     """
     wiersze, kolumny = plansza_rozmiar
-    wielkosc_obszaru = (wiersze * WIELKOSC + wiersze + 1,
-                        kolumny * WIELKOSC + kolumny + 1)
+    wielkosc_obszaru = (wiersze * odleglosc + 1,
+                        kolumny * odleglosc + 1)
     rysuj_obwodke(wielkosc_obszaru, surface)
     rysuj_linie_poziome(wiersze, surface)
     rysuj_linie_pionowe(kolumny, surface)
