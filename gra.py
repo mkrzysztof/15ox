6caf8a975d316e9a6267fbbcf338d76d3e6e8849
krzysio <krzysio@brzoskwinka.home>
2018-10-20 20:54:06 +0200

    wrównanie przycisków wyboru graczy  Committer: krzysio <krzysio@brzoskwinka.home>
     On branch popraw_efekty_graficzne
     Changes to be committed:
    	modified:   gra.py
    	modified:   plansza_wyboru_wid.py

--- gra.py
+++ gra.py
@@ -21,8 +21,8 @@ def pokaz_wybory():
     pww.dodaj_napisy_ile(surface)
     while not pwm.zatwierdzono_wybor:
         events = pygame.event.get()
-        pww.obsluz_przyciski_rozm(events, pww.PRZYCISKI_ZMIAN)
-        pww.obsluz_przyciski_ile(events, pww.PRZYCISKI_KTO_GRA)
+        pww.obsluz_przyciski(events, pww.PRZYCISKI_ZMIAN)
+        pww.obsluz_przyciski(events, pww.PRZYCISKI_KTO_GRA)
         pww.obsloz_OK(events, pww.PRZYCISK_OK)
         obsloz_zamkniecie(events)
     surface.fill(pygame.color.THECOLORS['black'])
--- plansza_wyboru_wid.py
+++ plansza_wyboru_wid.py
@@ -18,6 +18,7 @@ WYBOR_KTO_GRA = [pwm.wybierz_gracz_gracz, pwm.wybierz_komputer_gracz,
                  pwm.wybierz_gracz_komputer]
 FUN_KTO_GRA = dict(zip(KTO_GRA, WYBOR_KTO_GRA))
 
+
 def _obsluga_radio(funkcje_obslugi, przyciski, aktywna_opcja):
     # aktywuje wybraną aktywną opcje, czyści przyciski nie związane
     # z tą opcją
@@ -25,55 +26,52 @@ def _obsluga_radio(funkcje_obslugi, przyciski, aktywna_opcja):
     for k in (funkcje_obslugi.keys() - {aktywna_opcja}):
         przyciski[k].wyczysc()
 
-# obsługa zgrupowanych trzech przycisków
-def _funkcja_obslugi_roz(rozmiar):
-    _obsluga_radio(FUN_WYBOROW, PRZYCISKI_ZMIAN, rozmiar)
 
-def _funkcja_obslugi_ile(kto):
-    _obsluga_radio(FUN_KTO_GRA, PRZYCISKI_KTO_GRA, kto)
+# obsługa zgrupowanych trzech przycisków
 
 def _umiesc_radio(opcje, przyciski, pozycje, surface):
     for opcja, pozycja in zip(opcje, pozycje):
         przyciski[opcja].umiesc_na_pozycji(*pozycja, surface)
 
-def _umiesc_napisy(ROZMIARY_PLANSZY, pozycje_napisow, surface):
+def _umiesc_napisy(opcje, pozycje_napisow, surface):
     font = pygame.font.SysFont("", 30)
-    for rozmiar, pozycja in zip(ROZMIARY_PLANSZY, pozycje_napisow):
+    for rozmiar, pozycja in zip(opcje, pozycje_napisow):
         napis = font.render(rozmiar, False, pygame.color.THECOLORS['green'])
         surface.blit(napis, pozycja)
     pygame.display.flip()
-
-def _umiesc_przyciski_ile(ROZMIARY_PLANSZY, pozycje_przyc, surface):
-    pass
 
 def _dolacz_obsluge(opcje, przyciski, funkcja_obslugi):
     for opcja in opcje:
         przyciski[opcja].dodaj_obsluge(opcja, funkcja_obslugi, opcja)
 
-def _dolacz_obsluge_ile(ROZMIARY_PLANSZY, _funkcja_obslugi):
-    pass
-
-def obsluz_przyciski_rozm(events, przyciski):
+def obsluz_przyciski(events, przyciski):
     for rozmiar, przycisk in przyciski.items():
         przycisk.wykryj_klikniecie(events)
 
-def obsluz_przyciski_ile(events, przyciski):
-    for rozmiar, przycisk in przyciski.items():
-        przycisk.wykryj_klikniecie(events)
+def utworz_przyciski(opcje, przyciski, funkcja_obslugi,
+                     poz_x, poz_y, surface):
+    pozycje_przycisku = [(poz_x, poz_y + i * PIONOWY_ODSTEP)
+                         for i in range(len(opcje))]
+    _umiesc_radio(opcje, przyciski, pozycje_przycisku, surface)
+    _dolacz_obsluge(opcje, przyciski, funkcja_obslugi)
+    
         
 def utworz_przyciski_rozm(surface):
-    poz_x_przycisku = 50
-    pozycje_przyc = [(poz_x_przycisku, POZ_Y_POCZATKOWA + i * PIONOWY_ODSTEP)
-                     for i in range(len(ROZMIARY_PLANSZY))]
-    _umiesc_radio(ROZMIARY_PLANSZY, PRZYCISKI_ZMIAN, pozycje_przyc, surface)
-    _dolacz_obsluge(ROZMIARY_PLANSZY, PRZYCISKI_ZMIAN, _funkcja_obslugi_roz)
+    def _funkcja_obslugi_roz(rozmiar):
+        _obsluga_radio(FUN_WYBOROW, PRZYCISKI_ZMIAN, rozmiar)
+
+    utworz_przyciski(ROZMIARY_PLANSZY, PRZYCISKI_ZMIAN, _funkcja_obslugi_roz,
+                     50, POZ_Y_POCZATKOWA, surface)
+
+def dodaj_napisy(pozycja, opcje, surface):
+    poz_x_napisu, poz_y_napisu = pozycja
+    pozycje_napisow = [(poz_x_napisu, poz_y_napisu + i * PIONOWY_ODSTEP)
+                       for i in range(len(opcje))]
+    _umiesc_napisy(opcje, pozycje_napisow, surface)
 
 def dodaj_napisy_rozm(surface):
-    poz_x_napisu = 100
-    pozycje_napisow = [(poz_x_napisu, POZ_Y_POCZATKOWA + i * PIONOWY_ODSTEP)
-                       for i in range(len(ROZMIARY_PLANSZY))]
-    _umiesc_napisy(ROZMIARY_PLANSZY, pozycje_napisow, surface)
-
+    dodaj_napisy((100, POZ_Y_POCZATKOWA), ROZMIARY_PLANSZY, surface)
+    
 
 # przycisk OK
 
@@ -90,18 +88,15 @@ def obsloz_OK(events, PRZYCISK_OK):
 # przyciski wybór ilości
 
 def utworz_przyciski_ile(surface):
-    poz_x_przycisku = 120
-    pozycje_przyc = [(poz_x_przycisku, POZ_Y_POCZATKOWA + i * PIONOWY_ODSTEP)
-                     for i in range(len(KTO_GRA))]
-    _umiesc_radio(KTO_GRA, PRZYCISKI_KTO_GRA, pozycje_przyc, surface)
-    _dolacz_obsluge(KTO_GRA, PRZYCISKI_KTO_GRA, _funkcja_obslugi_ile)
+    def _funkcja_obslugi_ile(kto):
+        _obsluga_radio(FUN_KTO_GRA, PRZYCISKI_KTO_GRA, kto)
+
+    utworz_przyciski(KTO_GRA, PRZYCISKI_KTO_GRA, _funkcja_obslugi_ile,
+                     180, POZ_Y_POCZATKOWA, surface)
 
 def dodaj_napisy_ile(surface):
-    poz_x_napisu = 250
-    pozycje_napisow = [(poz_x_napisu, POZ_Y_POCZATKOWA + i * PIONOWY_ODSTEP)
-                       for i in range(len(KTO_GRA))]
-    _umiesc_napisy(KTO_GRA, pozycje_napisow, surface)
-
+    dodaj_napisy((250, POZ_Y_POCZATKOWA), KTO_GRA, surface)
+    
 
 if __name__ == "__main__":
     def main():
