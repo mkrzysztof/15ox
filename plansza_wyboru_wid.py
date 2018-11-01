d30af6ab7f0f6a45996fbfc0581204199ff74a2b
krzysio <krzysio@brzoskwinka.home>
2018-11-01 17:08:01 +0100

    skrócenie list bparametrów w funkcjaach modułu radio_wid ukrycie etykiet napisów etykiety są etykietami pól radio (przycisków)  Committer: krzysio <krzysio@brzoskwinka.home>
     On branch wydziel_radio
     Changes to be committed:
    	modified:   plansza_wyboru_wid.py
    	modified:   radio_wid.py

--- plansza_wyboru_wid.py
+++ plansza_wyboru_wid.py
@@ -7,27 +7,26 @@ import radio_wid
 #stałe
 
 POZ_Y_POCZATKOWA = 50
-ROZMIARY_PLANSZY = ['xo', '10x10', '15x15',]
-PRZYCISKI_ZMIAN = {k : zaznaczenie.PrzyciskGraf() for k in ROZMIARY_PLANSZY}
-WYBORY_ROZMIAROW = [pwm.wybierz_xo, pwm.wybierz_10x10, pwm.wybierz_15x15]
-FUN_WYBOROW = dict(zip(ROZMIARY_PLANSZY, WYBORY_ROZMIAROW))
+_ROZMIARY_PLANSZY = ['xo', '10x10', '15x15',]
+PRZYCISKI_ZMIAN = {k : zaznaczenie.PrzyciskGraf() for k in _ROZMIARY_PLANSZY}
+_WYBORY_ROZMIAROW = [pwm.wybierz_xo, pwm.wybierz_10x10, pwm.wybierz_15x15]
+FUN_WYBOROW = dict(zip(_ROZMIARY_PLANSZY, _WYBORY_ROZMIAROW))
 
-KTO_GRA = ['GRACZ - GRACZ', 'KOMPUTER - GRACZ', 'GRACZ - KOMPUTER']
-PRZYCISKI_KTO_GRA = {k: zaznaczenie.PrzyciskGraf() for k in KTO_GRA}
-WYBOR_KTO_GRA = [pwm.wybierz_gracz_gracz, pwm.wybierz_komputer_gracz,
+_KTO_GRA = ['GRACZ - GRACZ', 'KOMPUTER - GRACZ', 'GRACZ - KOMPUTER']
+PRZYCISKI_KTO_GRA = {k: zaznaczenie.PrzyciskGraf() for k in _KTO_GRA}
+_WYBOR_KTO_GRA = [pwm.wybierz_gracz_gracz, pwm.wybierz_komputer_gracz,
                  pwm.wybierz_gracz_komputer]
-FUN_KTO_GRA = dict(zip(KTO_GRA, WYBOR_KTO_GRA))
+FUN_KTO_GRA = dict(zip(_KTO_GRA, _WYBOR_KTO_GRA))
 
 PRZYCISK_OK = zaznaczenie.PrzyciskOK()
 
 def utworz_pole_radio(pola_radio, poz_x, funkcja_obslugi, surface):
     ODSTEP = 70
-    klucze = pola_radio.keys()
     pozycja = (poz_x, POZ_Y_POCZATKOWA)
-    radio_wid.utworz_radio(klucze, pola_radio, funkcja_obslugi,
+    radio_wid.utworz_radio(pola_radio, funkcja_obslugi,
                            pozycja, surface)
     pozycja = (poz_x + ODSTEP, POZ_Y_POCZATKOWA)
-    radio_wid.dodaj_napisy(pozycja, klucze, surface)
+    radio_wid.dodaj_napisy(pola_radio, pozycja, surface)
 
 def _funkcja_obslugi_roz(rozmiar):
     radio_wid.obsluga_radio(FUN_WYBOROW, PRZYCISKI_ZMIAN, rozmiar)
--- radio_wid.py
+++ radio_wid.py
@@ -12,41 +12,42 @@ def obsluga_radio(funkcje_obslugi, pola, aktywna_opcja):
 
 # obsługa zgrupowanych pól
 
-def _umiesc_radio(opcje, przyciski, pozycje, surface):
+def _umiesc_radio(przyciski, pozycje, surface):
+    opcje = przyciski.keys()
     for opcja, pozycja in zip(opcje, pozycje):
         przyciski[opcja].umiesc_na_pozycji(*pozycja, surface)
 
-def _umiesc_napisy(opcje, pozycje_napisow, surface):
+def _umiesc_napisy(pola_radio, pozycje_napisow, surface):
+    opcje = pola_radio.keys()
     font = pygame.font.SysFont("", 30)
     for rozmiar, pozycja in zip(opcje, pozycje_napisow):
         napis = font.render(rozmiar, False, pygame.color.THECOLORS['green'])
         surface.blit(napis, pozycja)
     pygame.display.flip()
 
-def _dolacz_obsluge(opcje, pola, funkcja_obslugi):
+def _dolacz_obsluge(pola, funkcja_obslugi):
+    opcje = pola.keys()
     for opcja in opcje:
         pola[opcja].dodaj_obsluge(opcja, funkcja_obslugi, opcja)
 
-def utworz_radio(opcje, pola, funkcja_obslugi,
-                     pozycja, surface):
+def utworz_radio(pola, funkcja_obslugi, pozycja, surface):
     """ tworzy zgrupowane pole wyboru typu 'radio' 
     Argumenty:
-    opcje -- lista opisów
-    pola -- lista pól typu PrzyciskiGraf
+    pola -- lista pól radio indeksowana ich opisami
     funkcja_obslugi -- funkcja obsługująca utworzone pole radio
     pozycja -- pozycja na utworzonym oknie graficznym
     surface -- okno graficzne """
     poz_x, poz_y = pozycja
     pozycje_radio = [(poz_x, poz_y + i * PIONOWY_ODSTEP)
-                         for i in range(len(opcje))]
-    _umiesc_radio(opcje, pola, pozycje_radio, surface)
-    _dolacz_obsluge(opcje, pola, funkcja_obslugi)
+                         for i in range(len(pola))]
+    _umiesc_radio(pola, pozycje_radio, surface)
+    _dolacz_obsluge(pola, funkcja_obslugi)
 
-def dodaj_napisy(pozycja, opcje, surface):
+def dodaj_napisy(pola, pozycja, surface):
     poz_x_napisu, poz_y_napisu = pozycja
     pozycje_napisow = [(poz_x_napisu, poz_y_napisu + i * PIONOWY_ODSTEP)
-                       for i in range(len(opcje))]
-    _umiesc_napisy(opcje, pozycje_napisow, surface)
+                       for i in range(len(pola))]
+    _umiesc_napisy(pola, pozycje_napisow, surface)
 
 def obsluz_radio(events, pola):
     for rozmiar, pole in pola.items():
