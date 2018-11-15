935e1357a2ad00aca7c16623bd7a59d1c04d9447
krzysio <>
2018-11-15 14:24:01 +0100

    Wierzcholek stał się podklasą dict
     On branch poprawa_wydajnosci
     Changes to be committed:
    	modified:   drzewo.py

--- drzewo.py
+++ drzewo.py
@@ -1,15 +1,6 @@
-class Wierzcholek:
+class Wierzcholek(dict):
     def __init__(self, stan_siatki, gracz):
+        super().__init__()
         self.siatka = stan_siatki
         self.gracz = gracz
         self.wartosc = None
-        self.__dzieci = {}
-
-    def __setitem__(self, klucz, wierzcholek):
-        self.__dzieci[klucz] = wierzcholek
-
-    def __getitem__(self, klucz):
-        return self.__dzieci[klucz]
-
-    def keys(self):
-        return self.__dzieci.keys()
