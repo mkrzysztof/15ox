8d022bdb8300d69bf6be6d631635582928350781
krzysio <>
2019-01-04 12:09:13 +0100

    napisano do końca parametry_test.py
     On branch nowe-alfa-beta
     Your branch is up to date with 'origin/nowe-alfa-beta'.
    
     Changes to be committed:
    	modified:   parametry_test.py

--- parametry_test.py
+++ parametry_test.py
@@ -61,11 +61,44 @@ class OgraniczeniaTest(unittest.TestCase):
         ogr.aktualizuj(parametry.Faza.ALFA, ocena, "y")
         self.assertEqual(ogr._ograniczenia[parametry.Faza.ALFA]._ruch, "y")
 
-    
+    def test_czy_ciecie(self):
+        ograniczenie_test = parametry.Ograniczenia()
+        oceny_alfa = [parametry.Ocena("a", 10), parametry.Ocena("b", 2)]
+        oceny_beta = [parametry.Ocena("c", 5), parametry.Ocena("d", 7)]
+        ograniczenia = [parametry.Ograniczenia(), parametry.Ograniczenia()]
+        for ogr, par_ogr in zip(ograniczenia, zip(oceny_alfa, oceny_beta)):
+            ogr_a, ogr_b = par_ogr
+            ogr.aktualizuj(parametry.Faza.ALFA, ogr_a, "x")
+            ogr.aktualizuj(parametry.Faza.BETA, ogr_b, "x")
+        wlasciwe = [True, False]
+        for ogr, wl_wyn in zip(ograniczenia, wlasciwe):
+            wynik = ogr.czy_ciecie()
+            self.assertEqual(wl_wyn, wynik)
+
+    def test_czy_aktualizowac(self):
+        rozpatrywane_fazy = [parametry.Faza.ALFA, parametry.Faza.ALFA,
+                             parametry.Faza.BETA, parametry.Faza.BETA]
+        ograniczenia = [parametry.Ograniczenia() for k in rozpatrywane_fazy]
+        oceny_alfa = [parametry.Ocena("x", 10), parametry.Ocena("x", 5),
+                      parametry.Ocena("x", 20), parametry.Ocena("x", 30)]
+        oceny_beta = [parametry.Ocena("x", 13), parametry.Ocena("x", 14),
+                      parametry.Ocena("x", -2), parametry.Ocena("x", -1)]
+        oceny = [parametry.Ocena("x", 20), # 20 > 10
+                 parametry.Ocena("x", 1), # 1 > 5 NIE !!!
+                 parametry.Ocena("x", -3), # -3 < -2
+                 parametry.Ocena("x", 1)] # 1 < -1 NIE !!!
+        wlasciwe_wyniki = [True, False, True, False]
+        for ogr, par_ogr in zip(ograniczenia, zip(oceny_alfa, oceny_beta)):
+            ogr_a, ogr_b = par_ogr
+            ogr.aktualizuj(parametry.Faza.ALFA, ogr_a, "x")
+            ogr.aktualizuj(parametry.Faza.BETA, ogr_b, "x")
+        for ogr, ocena, wl_wyn, faza in zip(ograniczenia, oceny,
+                                            wlasciwe_wyniki,
+                                            rozpatrywane_fazy):
+                 wynik = ogr.czy_aktualizowac(faza, ocena)
+                 self.assertEqual(wl_wyn, wynik)
 
 def main():
     unittest.main()
 if __name__ == "__main__":
     main()
-
-    
